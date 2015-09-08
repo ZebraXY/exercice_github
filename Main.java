5c6fe017755f220b6a5ab0975af80dc80eaa42b9
Zebra <nicolas.boeuf@gmail.com>
2015-09-09 00:24:08 +0200

    Correction methode fac() de la class Utils + ajout instruction dans la class Main

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		System.out.println("La valeur factorelle de 5 est :"+ Utils.fac(5));
 
 	}
 
--- Utils.java
+++ Utils.java
@@ -9,7 +9,7 @@ public class Utils {
 	 * @return n factoriel
 	 */
 	public static int fac(int n) {
-		if (n == 1)
+		if (n == 0)
 			return 1;
 		return n * fac(n - 1);
 
